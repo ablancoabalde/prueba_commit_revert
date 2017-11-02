0456725fab660919cfac7ef1a89a77939077ef1d
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2017-11-02 09:38:23 +0100

    funcion A

--- src/prueba/comit/revert/PruebaComitRevert.java
+++ src/prueba/comit/revert/PruebaComitRevert.java
@@ -4,7 +4,11 @@ package prueba.comit.revert;
 public class PruebaComitRevert {
 
     public static void main(String[] args) {
-        
+        int x =0;
+        if(x==2){
+            
+        }
+       
     }
     
 }
